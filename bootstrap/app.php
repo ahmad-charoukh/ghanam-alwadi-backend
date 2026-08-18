9aeac6ebbabd9991c76ce60db323dbf2eb5f9b9a
Ahmad Charoukh <ahmad-charoukh@users.noreply.github.com>
2026-08-18 11:56:48 +0300

    Trust Render proxy and generate HTTPS assets

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,9 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
+    ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+    })
+    ->withExceptions(function (Exceptions $exceptions): void {
         //
     })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+    ->create();
